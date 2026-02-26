cd348f3d2fd32a8af75fee1483c5ddb2aa355e3c
tommy <batxent@me.com>
2026-02-26 23:27:12 +0800

    initial

---
+++ x/tokenfactory/keeper/query.go
@@ -0,0 +1,7 @@
+package keeper
+
+import (
+	"mini-chakra/x/tokenfactory/types"
+)
+
+var _ types.QueryServer = Keeper{}
---
+++ x/tokenfactory/module/genesis_test.go
@@ -0,0 +1,30 @@
+package tokenfactory_test
+
+import (
+	"testing"
+
+	keepertest "mini-chakra/testutil/keeper"
+	"mini-chakra/testutil/nullify"
+	tokenfactory "mini-chakra/x/tokenfactory/module"
+	"mini-chakra/x/tokenfactory/types"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.TokenfactoryKeeper(t)
+	tokenfactory.InitGenesis(ctx, k, genesisState)
+	got := tokenfactory.ExportGenesis(ctx, k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
